3c343aec98ae9c5147cfba467fc6ba700952acf6
NanyaTK <herenfuyong02@gmail.com>
2023-11-01 22:53:10 +0900

    docs: add LICENSE

---
+++ src/L2/interfaces/loopback.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 NanyaTK ( https://github.com/NanyaTK )
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ * loopback.h
+ * Header of loopback.c
+ */
+
+#ifndef LOOPBACK_H
+#define LOOPBACK_H
+
+#include "../network.h"
+
+extern IFNET *LoopbackInit(void);
+
+#endif
